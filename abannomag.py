acf549fee64ed1b193f6b77d09b3874365695404
Superkroksjo2 <90190522+Superkroksjo2@users.noreply.github.com>
2021-11-29 13:13:40 +0100

    🚑

--- abannomag.py
+++ abannomag.py
@@ -1,13 +1,9 @@
-# kontant < 33
-# normal 33-66
-# plus < 66
-
 print ("Välkommen till Lars och Olofs mobilabbonemang")
 uppskattade_minuter = int(input("Hur många minuter uppskattar du att du ringer per månad?"))
 
 if uppskattade_minuter < 33:
-  print("Du vill ha kontantabonemang")
+  print("Vi Recomenderar kontantabonemang")
 elif uppskattade_minuter < 66:
-  print("Du vill ha normalabonemang")
+  print("Vi Recomenderar det normala abonemanget")
 else:
-  print("Du vill ha plusabonemang")
+  print("Vi Recomenderar ett plusabonemang")
